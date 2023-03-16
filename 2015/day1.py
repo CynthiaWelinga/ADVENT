1959e0d1b78211dbb7ddadfe5f7e88f71f84722d
Cynthia Welinga <cyndyamina@gmail.com>
2023-03-15 20:16:25 -0400

    day 1advent

--- 2015/day1.py
+++ 2015/day1.py
@@ -1,7 +1,11 @@
 """
-Santa is trying to deliver presents in a large apartment building, but he can't find the right floor - the directions he got are a little confusing. He starts on the ground floor (floor 0) and then follows the instructions one character at a time.
+Santa is trying to deliver presents in a large apartment building, 
+but he can't find the right floor - the directions he got are a little confusing. 
 
-An opening parenthesis, (, means he should go up one floor, and a closing parenthesis, ), means he should go down one floor.
+He starts on the ground floor (floor 0) and then follows the instructions one character at a time.
+
+An opening parenthesis, (, means he should go up one floor, and 
+a closing parenthesis, ), means he should go down one floor.
 
 The apartment building is very tall, and the basement is very deep; he will never find the top or bottom floors.
 
@@ -14,3 +18,9 @@ For example:
 ))) and )())()) both result in floor -3.
 To what floor do the instructions take Santa?
 """
+
+import requests
+
+file_path = 'C:/Users/CynthiaWelinga/DEVELOPMENT/ADVENT/2015/day1.txt'
+with open(file_path) as file:
+    print(file.read())
