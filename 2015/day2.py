4681e50b5c02545f947445e7ec9dd5d976ba0cf9
Cynthia Welinga <cyndyamina@gmail.com>
2023-03-16 23:17:22 -0400

    ribbons

--- 2015/day2.py
+++ 2015/day2.py
@@ -21,33 +21,58 @@ import requests
 
 file_path = 'C:/Users/CynthiaWelinga/DEVELOPMENT/ADVENT/2015/day2.txt'
 puzzle_input = open(file_path).readlines()
-dimensions = []
-total = 0
 
 
-for i in puzzle_input:
-    dimensions.append(i.strip())
+def get_wrap():
+    dimensions = []
+    total = 0
+    for i in puzzle_input:
+        dimensions.append(i.strip())
 
-for item in dimensions:
-    item = sorted(item.split('x'))
-    for num in item:
+    for item in dimensions:
+        item = sorted(item.split('x'))
         nums =[int(num) for num in item]
-    nums.sort()
+        nums.sort()
 
+        total += (2*(nums[0]*nums[1]) + 2*(nums[1]*nums[2]) + 2*(nums[2]*nums[0])) + (nums[0]*nums[1])
 
-    total += (2*(nums[0]*nums[1]) + 2*(nums[1]*nums[2]) + 2*(nums[2]*nums[0])) + (nums[0]*nums[1])
-
-    
-print(total)
+        
+    print(total)
 
 
 
 """
-The ribbon required to wrap a present is the shortest distance around its sides, or the smallest perimeter of any one face. Each present also requires a bow made out of ribbon as well; the feet of ribbon required for the perfect bow is equal to the cubic feet of volume of the present. Don't ask how they tie the bow, though; they'll never tell.
+The ribbon required to wrap a present is the shortest distance around its sides, 
+or the smallest perimeter of any one face. 
+
+Each present also requires a bow made out of ribbon as well;
+the feet of ribbon required for the perfect bow is equal to the cubic feet of volume of the present. 
+
+Don't ask how they tie the bow, though; they'll never tell.
 
 For example:
 
-A present with dimensions 2x3x4 requires 2+2+3+3 = 10 feet of ribbon to wrap the present plus 2*3*4 = 24 feet of ribbon for the bow, for a total of 34 feet.
-A present with dimensions 1x1x10 requires 1+1+1+1 = 4 feet of ribbon to wrap the present plus 1*1*10 = 10 feet of ribbon for the bow, for a total of 14 feet.
+A present with dimensions 2x3x4 requires 2+2+3+3 = 10 feet of ribbon to wrap the present 
+plus 2*3*4 = 24 feet of ribbon for the bow, for a total of 34 feet.
+A present with dimensions 1x1x10 requires 1+1+1+1 = 4 feet of ribbon to wrap the present 
+plus 1*1*10 = 10 feet of ribbon for the bow, for a total of 14 feet.
 How many total feet of ribbon should they order?
 """
+
+def get_ribbon():
+    dimensions = []
+    total = 0
+    for i in puzzle_input:
+        dimensions.append(i.strip())
+
+    for item in dimensions:
+        item = sorted(item.split('x'))
+        nums =[int(num) for num in item]
+        nums.sort()
+
+        total = 2*nums[0] + 2*nums[1]
+
+    print(total)
+
+get_wrap()
+get_ribbon()
